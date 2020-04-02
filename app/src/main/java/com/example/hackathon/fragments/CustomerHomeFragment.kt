87b6cb007db91f12d3e4ec3518b6da85dd3c9271
StrangeNoob <sipun63427452@gmail.com>
2020-04-03 02:47:57 +0530

    Add Location Permission in Android

--- app/src/main/java/com/example/hackathon/fragments/CustomerHomeFragment.kt
+++ app/src/main/java/com/example/hackathon/fragments/CustomerHomeFragment.kt
@@ -6,9 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.hackathon.R
+import com.example.hackathon.activities.LoginActivity.Companion.latlng
 
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
@@ -16,7 +20,8 @@ import kotlinx.android.synthetic.main.fragment_customer_home.*
 
 class CustomerHomeFragment : Fragment() {
 
-    private val mapView: MapView? = null
+//    private val mapview: MapView? = null
+    private  val mapboxMap: MapboxMap ? =null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,19 +38,26 @@ class CustomerHomeFragment : Fragment() {
      override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-         mapview.onCreate(savedInstanceState)
 
+         mapview.onCreate(savedInstanceState)
          // Initializing is asynchrounous- getMapAsync will return a map
          mapview.getMapAsync { map ->
              // Set one of the many styles available
              map.setStyle(Style.OUTDOORS) { style ->
-                 // Style.MAPBOX_STREETS | Style.SATELLITE etc...
+                  Style.MAPBOX_STREETS //| Style.SATELLITE etc...
              }
          }
+         val position = CameraPosition.Builder()
+             .target(latlng)
+             .zoom(10.0)
+             .tilt(20.0)
+             .build()
+         mapboxMap?.animateCamera(CameraUpdateFactory.newCameraPosition(position), 10000)
      }
     public override fun onStart() {
         super.onStart()
         mapview.onStart()
+
     }
 
     public override fun onResume() {

2b03da25daad425026c66e29c610e31ac0ebac5f
StrangeNoob <sipun63427452@gmail.com>
2020-04-02 21:55:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/hackathon/activities/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.example.hackathon.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hackathon.MainActivity
+import com.example.hackathon.R
+
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //hiding title bar of this activity
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+        //4second splash time
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(applicationContext, LoginActivity::class.java))
+            //finish this activity
+            finish()
+        },10000)
+
+
+    }
+
+}

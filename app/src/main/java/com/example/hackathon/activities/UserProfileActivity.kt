ce782c6e537871cd97159bda2e6b2560c94a1110
Dwarikanathmishra <dwarikanathmishra7@gmail.com>
2020-04-05 16:26:53 +0530

    added layout for customerprofile

---
+++ app/src/main/java/com/example/hackathon/activities/UserProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.example.hackathon.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hackathon.R
+
+class UserProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}

981cf21823f45a53d95af5a73b80c335c563e97e
StrangeNoob <sipun63427452@gmail.com>
2020-04-18 13:07:55 +0530

    Add Call Functionality to View Shop Profile

--- app/src/main/java/com/example/hackathon/activities/ViewShopProfile.kt
+++ app/src/main/java/com/example/hackathon/activities/ViewShopProfile.kt
@@ -1,23 +1,32 @@
 package com.example.hackathon.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.bumptech.glide.Glide
 import com.example.hackathon.R
 import kotlinx.android.synthetic.main.activity_view_shop_profile.*
 
+
 class ViewShopProfile : AppCompatActivity() {
 
+    lateinit var  ShopPhoneNo: String
+    var ShopLocationLat : Double ?= null
+    var ShopLocationLang : Double ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_shop_profile)
         val shopName =  intent.getStringExtra("ShopName")
         val ShopOwnerName = intent.getStringExtra("ShopOwnerName")
         val ShopCategories = intent.getStringExtra("ShopCategory")
-        val ShopPhoneNo = intent.getStringExtra("ShopPhonNo")
-        val ShopLocationLat=intent.getDoubleExtra("ShopLocationLat",0.0)
-        val ShopLocationLang = intent.getDoubleExtra("ShopLocationLang",0.0)
+        ShopPhoneNo = intent.getStringExtra("ShopPhonNo")
+        ShopLocationLat=intent.getDoubleExtra("ShopLocationLat",0.0)
+        ShopLocationLang = intent.getDoubleExtra("ShopLocationLang",0.0)
         val ShopTime = intent.getStringExtra("ShopTiming")
         val ShopEmail = intent.getStringExtra("ShopEmail")
         val ShopImage = intent.getStringExtra("ShopImage")
@@ -29,9 +38,51 @@ class ViewShopProfile : AppCompatActivity() {
         viewShopOwner.text = ShopOwnerName
         viewShopEmail.text = ShopEmail
         viewShopTiming.text = ShopTime
-        viewShopPhone.text = ShopPhoneNo
         backBtn.setOnClickListener {
             onBackPressed()
         }
+        viewPhone.setOnClickListener {
+
+            if (ActivityCompat.checkSelfPermission(this,Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions()
+            }
+            val u: Uri = Uri.parse("tel:" + ShopPhoneNo)
+            val intent : Intent = Intent(Intent.ACTION_CALL)
+            intent.setData(u)
+            startActivity(intent)
+        }
     }
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CALL_PHONE),
+            1
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            1 -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("User Details", "Permission has been denied by user")
+
+                } else {
+                    Log.i("User Details", "Permission has been granted by user")
+                    val u: Uri = Uri.parse("tel:$ShopPhoneNo")
+                    val intent : Intent = Intent(Intent.ACTION_CALL)
+                    intent.setData(u)
+                    if (ActivityCompat.checkSelfPermission(
+                            this@ViewShopProfile,
+                            Manifest.permission.CALL_PHONE
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        return
+                    }
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+
 }
